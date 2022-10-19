56af16c5f91f2fae596c09e775ab928900f7c3ed
Mel <brandon.melend14@gmail.com>
2022-10-19 13:41:09 -0400

    Completed v1 of library

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function Book(title,author,pages,status){
     this.pages = pages;
     this.status = Boolean(status);
 }
+
+Book.prototype.changeStatus = function(){
+    this.status === true? this.status = false: this.status = true;
+}
 function removeBook(e){
     
     library = library.filter(book=>{
@@ -40,12 +44,22 @@ function bookCard(book){
 
     const statusElement = document.createElement('div');
     statusElement.classList.add("status-check");
-    statusElement.innerText= "Finished? ";
 
-    const statusInput = document.createElement('input');
-    statusInput.type = 'checkbox';
+    const statusInput = document.createElement('button');
+    statusInput.type = 'button';
 
+    setStatusButton(statusInput,book);
+    book.status === true? statusInput.innerText = "Read": statusInput.innerText = "Not Read";
+    statusInput.addEventListener('click',() => {
+        book.changeStatus();
+        setStatusButton(statusInput,book);
+        console.log(book)
+    });
+    
     statusElement.append(statusInput);
+
+    
+
     newBook.append(closeElement);
     newBook.append(titleElement);
     newBook.append(authorElement);
@@ -76,19 +90,61 @@ function loadBooks(){
     addBookCard();
 }
 function addToLibrary(form){
-
-    const book = new Book(form.title.value,form.author.value,form.pages.value,form.status.checked);
-    library.push(book);
-    loadBooks();
-    formContainer.setAttribute("style", "visibility: hidden;");
-   
+    
+    errorCheck(form);
+    if (form.title.value.length > 0 && form.author.value.length > 0 && form.pages.value.length > 0){ 
+        const book = new Book(form.title.value,form.author.value,form.pages.value,form.status.checked);
+        library.push(book);
+        loadBooks();
+        formContainer.setAttribute("style", "visibility: hidden;");
+    }
+    
 }
 
+function errorCheck(form){
+    if(form.title.value.length < 1){
+        const titleError = document.querySelector('.title-error');
+        titleError.setAttribute("style", "visibility: visible;");
+    }else{
+        const titleError = document.querySelector('.title-error');
+        titleError.setAttribute("style", "visibility: hidden;");
+    }
+
+    if (form.author.value.length <1){
+        const authorError = document.querySelector('.author-error');
+        authorError.setAttribute("style", "visibility: visible;");
+    }else{
+        const authorError = document.querySelector('.author-error');
+        authorError.setAttribute("style", "visibility: hidden;");
+    }
+
+    if (form.pages.value.length <1){
+        const pagesError = document.querySelector('.page-error');
+        pagesError.setAttribute("style", "visibility: visible;");
+
+    }else{
+        const pagesError = document.querySelector('.page-error');
+        pagesError.setAttribute("style", "visibility: hidden;");
+    }
+}
+
+function setStatusButton(btn,book){
+    if(book.status){
+        btn.classList.remove('not-read-btn');
+        btn.classList.add('read-btn');
+        btn.innerText = "Read";
+    }else{
+        btn.classList.remove('read-btn');
+        btn.classList.add('not-read-btn');
+        btn.innerText = "Not Read";
+    }
+}
 
 const addBook = document.querySelector("#addBook");
 const formContainer = document.querySelector(".form-container");
 const bookForm = document.querySelector("#book-form").elements;
 const submit = document.querySelector('#add');
+const closeForm = document.querySelector('.closeButton-form');
 
 // const removeCard = document.querySelector("#closeButton");
 // removeCard.
@@ -98,6 +154,8 @@ addBook.addEventListener('click',()=>{
     document.getElementById("book-form").reset();
     formContainer.setAttribute("style", "visibility: visible;");
 })
-
+closeForm.addEventListener('click',()=>{
+    formContainer.setAttribute("style", "visibility: hidden;");  
+})
 
 // console.log(library);
